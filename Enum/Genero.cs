1d361408d903dad679b4e007793069d3c13fadef
Diego Leonardo Barbosa <72359859+DiegoLBC@users.noreply.github.com>
2021-09-26 22:11:43 -0300

    Add files via upload

---
+++ Enum/Genero.cs
@@ -0,0 +1,17 @@
+namespace DIO.Animes
+{
+    public enum Genero
+    {
+        Shonen = 1,
+        Shoujo = 2,
+        Seinen = 3,
+        Josei = 4,
+        Isekai = 5,
+        Mecha = 6,
+        SliceOfLife = 7,
+        Kodomuke = 8,
+        Iyashikei = 9
+
+
+    }
+}
